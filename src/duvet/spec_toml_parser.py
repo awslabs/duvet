fa444e3f09ab84847768055f5f806699ae252934
Yuan Chen <18861720589@163.com>
2022-06-16 16:00:14 -0700

    feat(Parser): Spec TOML Parser

--- src/duvet/spec_toml_parser.py
+++ src/duvet/spec_toml_parser.py
@@ -1,8 +1,8 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Specification Parser used by duvet-python for toml format."""
-from pathlib import Path
 import warnings
+from pathlib import Path
 from typing import Dict, List, Optional
 
 import toml
