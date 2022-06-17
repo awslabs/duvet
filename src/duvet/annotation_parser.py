c1b11162b4c0b4d209e5f5633fcc3a62b9952eb2
Yuan Chen <18861720589@163.com>
2022-06-16 23:02:09 -0700

    feat(Parser): Annotation Parser

--- src/duvet/annotation_parser.py
+++ src/duvet/annotation_parser.py
@@ -1,17 +1,15 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Annotation Parser used by duvet-python."""
-import re
-
 import pathlib
+import re
+from typing import List
 
 from attr import field
 from attrs import define
-from typing import List, Dict
 
-from duvet._config import DEFAULT_META_STYLE, DEFAULT_CONTENT_STYLE
+from duvet._config import DEFAULT_CONTENT_STYLE, DEFAULT_META_STYLE
 from duvet.identifiers import AnnotationType
-from src.duvet.identifiers import *
 from duvet.structures import Annotation
 
 __all__ = ["AnnotationParser"]
@@ -38,16 +36,22 @@ TEST_DAFNY_STR = (
 
 
 @define
-class AnnotationParser():
+class AnnotationParser:
+    """Parser for annotation from implementation."""
+
     paths: [pathlib.Path] = field(init=True)
     annotations: List[Annotation] = field(init=False, default=[])
     meta_style: str = DEFAULT_META_STYLE
     content_style: str = DEFAULT_CONTENT_STYLE
 
     def extract_file_annotations(self, file_path: pathlib.Path) -> List[Annotation]:
+        """Given a path of a implementation code.
+
+        Return a list of annotation objects.
+        """
         temp_annotation_list = []
-        implementation_file = open(file_path, "r")
-        lines = implementation_file.readlines()
+        with open(file_path, "r", encoding="utf-8") as implementation_file:
+            lines = implementation_file.readlines()
         curr_line = 0
         annotation_start = -1
         annotation_end = -1
@@ -61,14 +65,16 @@ class AnnotationParser():
                     annotation_end = curr_line
             else:
                 temp_annotation_list.append(
-                    self._extract_annotation_block(lines, annotation_start, annotation_end, file_path))
+                    self._extract_annotation_block(lines, annotation_start, annotation_end, file_path)
+                )
                 annotation_start = -1
                 annotation_end = -1
             curr_line += 1
         return temp_annotation_list
 
-    def _extract_annotation_block(self, lines: List[str], annotation_start: int, annotation_end: int,
-                                  file_path: pathlib.Path) -> Annotation:
+    def _extract_annotation_block(
+            self, lines: List[str], annotation_start: int, annotation_end: int, file_path: pathlib.Path
+    ) -> Annotation:
         new_lines = "".join(lines[annotation_start:annotation_end])
         return self._extract_annotation(new_lines, annotation_start, annotation_start, file_path)
 
@@ -80,15 +86,16 @@ class AnnotationParser():
         We will not support it for now.
         """
 
-        anno_type_regex = re.compile(self.meta_style + "[\s]type=" + "(.*?)\n")
+        anno_type_regex = re.compile(self.meta_style + r"[\s]type=" + r"(.*?)\n")
         temp_type = re.search(anno_type_regex, lines).group(1).upper()
         # temp_type = re.search(r'//=\stype=(.*?)\n', lines).group(1).upper()
         anno_type = AnnotationType[temp_type]
         anno_content = ""
-        target_meta = re.search(r'//=\s(.*?)\n', lines).group(1)
-        if re.findall(r'//#\s(.*?)\n', lines) is not None:
-            for temp_content in re.findall(r'//#\s(.*?)\n', lines):
+        target_meta = re.search(r"//=\s(.*?)\n", lines).group(1)
+        if re.findall(r"//#\s(.*?)\n", lines) is not None:
+            for temp_content in re.findall(r"//#\s(.*?)\n", lines):
                 anno_content = "".join([anno_content, temp_content])
-        anno_content = anno_content.replace("\n","")
-        return Annotation(target_meta, anno_type, anno_content, start, end, "$".join([target_meta, anno_content]),
-                          file_path.resolve())
+        anno_content = anno_content.replace("\n", "")
+        return Annotation(
+            target_meta, anno_type, anno_content, start, end, "$".join([target_meta, anno_content]), file_path.resolve()
+        )
--- test/unit/test_annotation_parser.py
+++ test/unit/test_annotation_parser.py
@@ -6,8 +6,6 @@ import pathlib
 import pytest
 
 from duvet.annotation_parser import AnnotationParser
-from src.duvet.identifiers import *
-from src.duvet.annotation_parser import *
 
 pytestmark = [pytest.mark.unit, pytest.mark.local]
 
@@ -34,14 +32,11 @@ def test_extract_annotation():
         "a Key Commitment (../framework/algorithm-suites.md#algorithm-\n"
         ""
         "suites-encryption-key-derivation-settings) value of False\n"
-    ).replace("\n","")
-    actual_anno = AnnotationParser(
-        [pathlib.Path("test.dfy")])._extract_annotation(
-        TEST_DAFNY_STR, 0, 4,
-        pathlib.Path("test.dfy"))
-    assert actual_anno.type == AnnotationType["IMPLICATION"]
-    assert actual_anno.target == 'compliance/client-apis/client.txt#2.4.2.1'
+    ).replace("\n", "")
+    actual_anno = AnnotationParser([pathlib.Path("test.dfy")])._extract_annotation(
+        TEST_DAFNY_STR, 0, 4, pathlib.Path("test.dfy")
+    )
+    assert actual_anno.type.name == "IMPLICATION"
+    assert actual_anno.target == "compliance/client-apis/client.txt#2.4.2.1"
     assert actual_anno.content == expected_content
-    assert actual_anno.uri == "$".join([
-        "compliance/client-apis/client.txt#2.4.2.1",
-        expected_content])
+    assert actual_anno.uri == "$".join(["compliance/client-apis/client.txt#2.4.2.1", expected_content])
