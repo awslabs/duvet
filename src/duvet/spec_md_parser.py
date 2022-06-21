6b7c6026f494901c1fc589b9ae9beb0057143eae
Yuan Chen <18861720589@163.com>
2022-06-20 21:07:22 -0700

    feat(Markdown Parser): Read from markdown spec and write to toml

--- src/duvet/spec_md_parser.py
+++ src/duvet/spec_md_parser.py
@@ -8,10 +8,7 @@ from typing import List, Optional
 import toml
 from attrs import define
 
-from duvet.requirement_parser import (
-    RequirementParser,
-    create_requirements_from_list_to_section,
-)
+from duvet.requirement_parser import RequirementParser, create_requirements_from_list_to_section
 from duvet.structures import Report, Section, Specification
 
 __all__ = ["MDRequirementParser"]
@@ -53,8 +50,9 @@ class MDSpec:
         heading4 = "h4"
         curr_line = 0
         spec = Specification(markdown_spec_dir.name, str(os.path.relpath(markdown_spec_dir, pathlib.Path.cwd())))
-        curr_section = Section(heading4, "#".join([spec.spec_dir, heading4.rsplit(".", maxsplit=1)[-1]]), curr_line,
-                               curr_line)
+        curr_section = Section(
+            heading4, "#".join([spec.spec_dir, heading4.rsplit(".", maxsplit=1)[-1]]), curr_line, curr_line
+        )
         # for this toy script we only support update to heading4
         # which would be an equivalent to rfc in 1.1.1
         while curr_line < len(lines):
@@ -65,21 +63,24 @@ class MDSpec:
                 # set the end line of the previous section
                 curr_section.end_line = curr_line - 1
                 # set the start line of the new section
-                curr_section = Section(heading4, "#".join([spec.spec_dir, heading4.split(".")[-1]]), curr_line,
-                                       curr_line)
+                curr_section = Section(
+                    heading4, "#".join([spec.spec_dir, heading4.split(".")[-1]]), curr_line, curr_line
+                )
                 # turn the new section to curr_section
                 spec.sections.setdefault(heading4, curr_section)
                 # print(target)
                 # print(heading4)
             elif "### " in line:
                 target = line.replace("### ", heading1 + ".").replace(" ", "-").removesuffix("\n").lower()
-                heading3 = line.replace("### ", heading2 + ".").replace(" ", "-").removesuffix("\n").replace("/",
-                                                                                                             "").lower()
+                heading3 = (
+                    line.replace("### ", heading2 + ".").replace(" ", "-").removesuffix("\n").replace("/", "").lower()
+                )
                 # set the end line of the previous section
                 curr_section.end_line = curr_line - 1
                 # set the start line of the new section
-                curr_section = Section(heading3, "#".join([spec.spec_dir, heading3.split(".")[-1]]), curr_line,
-                                       curr_line)
+                curr_section = Section(
+                    heading3, "#".join([spec.spec_dir, heading3.split(".")[-1]]), curr_line, curr_line
+                )
                 # turn the new section to curr_section
                 spec.sections.setdefault(heading3, curr_section)
                 # print(target)
@@ -90,8 +91,10 @@ class MDSpec:
                 curr_section.end_line = curr_line - 1
                 # set the start line of the new section
                 curr_section = Section(
-                    heading2, "#".join([str(markdown_spec_dir.resolve()), heading2.split(".")[-1]]), curr_line,
-                    curr_line
+                    heading2,
+                    "#".join([str(markdown_spec_dir.resolve()), heading2.split(".")[-1]]),
+                    curr_line,
+                    curr_line,
                 )
                 # turn the new section to curr_section
                 spec.sections.setdefault(heading2, curr_section)
@@ -115,7 +118,7 @@ class MDSpec:
         #         section_curr_line += 1
         #     section.requirements = reqs
         def _extract_requirements(section: Section, lines: List[str]) -> bool:
-            quotes = "".join(lines[section.start_line: section.end_line])
+            quotes = "".join(lines[section.start_line : section.end_line])
             req_list_str = RequirementParser().extract_requirements(quotes)
             return create_requirements_from_list_to_section(section, req_list_str)
 
