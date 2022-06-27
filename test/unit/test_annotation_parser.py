24a599f8e280ee7175b8b3675b76bb808f3caa18
Yuan Chen <18861720589@163.com>
2022-06-27 13:11:04 -0700

    feat(annotation parser): clean coverage comments

--- test/unit/test_annotation_parser.py
+++ test/unit/test_annotation_parser.py
@@ -1,198 +1,98 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Annotation Parser used by duvet-python."""
-from test.utils import populate_file
-
 import pytest
 
+from duvet._config import DEFAULT_CONTENT_STYLE, DEFAULT_META_STYLE
 from duvet.annotation_parser import AnnotationParser, LineSpan
 
 pytestmark = [pytest.mark.unit, pytest.mark.local]
 
-TEST_DFY_BLOCK = """        //= compliance/client-apis/client.txt#2.4
-        //= type=implication
-        //# On client initialization, the caller MUST have the option to provide
-        //# a:
-        //#*  commitment policy (Section 2.4.1)
-        //#*  maximum number of encrypted data keys (Section 2.4.2)
-"""
 
-ANNOTATION_NESTED_IN_FUNCTION = """
-  function method IVSeq(suite: Client.AlgorithmSuites.AlgorithmSuite, sequenceNumber: uint32)
-    :(ret: seq<uint8>)
-    //= compliance/data-format/message-body.txt#2.5.2.1.2
-    //= type=implication
-    //# The IV length MUST be equal to the IV
-    //# length of the algorithm suite specified by the Algorithm Suite ID
-    //# (message-header.md#algorithm-suite-id) field.
-    //= compliance/data-format/message-body.txt#2.5.2.2.3
-    //= type=implication
-    //# The IV length MUST be equal to the IV length of the algorithm suite
-    //# (../framework/algorithm-suites.md) that generated the message.
-    ensures |ret| == suite.encrypt.ivLength as nat
-  {
-    seq(suite.encrypt.ivLength as int - 4, _ => 0) + UInt32ToSeq(sequenceNumber)
-  }"""
-
-ANNOTATION_END_OF_FILE = """
-  //= compliance/data-format/message-body.txt#2.5.2.1.2
-  //= type=implication
-  //# Each frame in the Framed Data (Section 2.5.2) MUST include an IV that
-  //# is unique within the message.
-  //
-  //= compliance/data-format/message-body.txt#2.5.2.2.3
-  //= type=implication
-  //# The IV MUST be a unique IV within the message."""
+@pytest.fixture
+def under_test(tmp_path) -> AnnotationParser:
+    return AnnotationParser([tmp_path])
 
 
-def test_extract_blocks(tmp_path):
-    actual_path = populate_file(tmp_path, TEST_DFY_BLOCK, "src/test-duvet/test-duvet.dfy")
-    lines = TEST_DFY_BLOCK.splitlines(keepends=True)
-    parser = AnnotationParser(actual_path)
-    actual_linespan = parser._extract_blocks(lines)
-    assert actual_linespan == [LineSpan(start=0, end=6)]
-
-
-def test_extract_blocks_nested(tmp_path):
-    actual_path = populate_file(tmp_path, ANNOTATION_NESTED_IN_FUNCTION, "src/test-duvet/test-duvet.dfy")
-    lines = ANNOTATION_NESTED_IN_FUNCTION.splitlines(keepends=True)
-    parser = AnnotationParser(actual_path)
-    actual_linespan = parser._extract_blocks(lines)
-    assert actual_linespan == [LineSpan(start=3, end=12)]
-
-
-def test_extract_anno_kwargs(tmp_path):
-    actual_path = populate_file(tmp_path, TEST_DFY_BLOCK, "src/test-duvet/test-duvet.dfy")
-    lines = TEST_DFY_BLOCK.splitlines(keepends=True)
-    parser = AnnotationParser(actual_path)
-    line_span = LineSpan(0, 6)
-    actual_kwargs = parser._extract_anno_kwargs(lines, [line_span])
-    assert actual_kwargs == [
-        {
-            "content": "On client initialization, the caller MUST have the option to "
-            "provide "
-            "a: "
-            "*  commitment policy (Section 2.4.1) "
-            "*  maximum number of encrypted data keys (Section 2.4.2)",
-            "end_line": 6,
-            "reason": None,
-            "start_line": 0,
-            "target": "compliance/client-apis/client.txt#2.4",
-            "type": "implication",
-        }
-    ]
-
-
-def test_process_anno_kwargs(tmp_path):
-    actual_path = populate_file(tmp_path, ANNOTATION_NESTED_IN_FUNCTION, "src/test-duvet/test-duvet.dfy")
-    parser = AnnotationParser([actual_path])
-    actual_dicts = [
-        {
-            "content": "The IV length MUST be equal to the IV "
-            "length of the algorithm suite specified by the Algorithm Suite "
-            "ID "
-            "(message-header.md#algorithm-suite-id) field.",
-            "end_line": 8,
-            "reason": None,
-            "start_line": 3,
-            "target": "compliance/data-format/message-body.txt#2.5.2.1.2",
-            "type": "implication",
-        },
-        {
-            "content": "The IV length MUST be equal to the IV length of the algorithm "
-            "suite "
-            "(../framework/algorithm-suites.md) that generated the message.",
-            "end_line": 12,
-            "reason": None,
-            "start_line": 8,
-            "target": "compliance/data-format/message-body.txt#2.5.2.2.3",
-            "type": "implication",
-        },
-    ]
-    actual_annos = parser._process_anno_kwargs(actual_dicts, actual_path)
-    assert len(actual_annos) == 2
-    assert actual_annos[0].type.name == "IMPLICATION"
-    assert actual_annos[1].type.name == "IMPLICATION"
-    assert actual_annos[0].target == "compliance/data-format/message-body.txt#2.5.2.1.2"
-    assert actual_annos[1].target == "compliance/data-format/message-body.txt#2.5.2.2.3"
-    assert (
-        actual_annos[0].content
-        == "The IV length MUST be equal to the IV length of the algorithm suite specified by the "
-        "Algorithm Suite ID (message-header.md#algorithm-suite-id) field."
+class TestExtractSpans:
+    @pytest.mark.parametrize(
+        "text, expected_spans",
+        [
+            (f"\n{DEFAULT_META_STYLE} content\n", [LineSpan(1, 2)]),
+            (f"\n{DEFAULT_CONTENT_STYLE} content\n", [LineSpan(1, 2)]),
+            (f"{DEFAULT_CONTENT_STYLE} content\n", [LineSpan(0, 1)]),
+            (f"\n{DEFAULT_CONTENT_STYLE} content", [LineSpan(1, 2)]),
+            (f"\ncontent\n", []),  # noqa
+            (f"\n{DEFAULT_META_STYLE} content\n{DEFAULT_CONTENT_STYLE} content\n", [LineSpan(1, 3)]),
+            (f"\n{DEFAULT_META_STYLE} content\n\n{DEFAULT_CONTENT_STYLE} content\n", [LineSpan(1, 2), LineSpan(3, 4)]),
+        ],
     )
-    # Verify the last annotation is not broken.
-    assert (
-        actual_annos[1].content == "The IV length MUST be equal to the IV length of the algorithm "
-        "suite (../framework/algorithm-suites.md) that generated the message."
-    )
-    assert actual_annos[0].uri == (
-        "compliance/data-format/message-body.txt#2.5.2.1.2$The IV length MUST be "
-        "equal to the IV length of the algorithm suite specified by the Algorithm "
-        "Suite ID (message-header.md#algorithm-suite-id) field."
-    )
+    def test_extract(self, under_test, text: str, expected_spans: list[LineSpan]):
+        lines = text.splitlines(keepends=True)
+        actual_spans = under_test._extract_blocks(lines)
+        assert expected_spans == actual_spans
 
 
-#
-# def test_esdk_compliance_exceptions():
-#     logging.basicConfig(level=logging.INFO)
-#
-#     esdk_dafny = Path("/Users/tonyknap/workplace/Polymorph/aws-encryption-sdk-dafny")
-#     excep_paths: Iterable[Path] = esdk_dafny.glob("compliance_exceptions/**/*.txt")
-#     list_list_kwargs: list[list[dict]] = []
-#     a_parser = AnnotationParser(paths=excep_paths)
-#     for filepath in a_parser.paths:
-#         try:
-#             list_list_kwargs.append(a_parser.process_file(filepath))
-#         except KeyboardInterrupt:
-#             break
-#         except Exception as ex:
-#             _LOGGER.error("%s: hit %s.", (str(filepath), ex), ex)
-#
-#
-# def test_end_file():
-#     annotation_end_of_file = """
-#       //= compliance/data-format/message-body.txt#2.5.2.1.2
-#       //= type=implication
-#       //# Each frame in the Framed Data (Section 2.5.2) MUST include an IV that
-#       //# is unique within the message.
-#       //
-#       //= compliance/data-format/message-body.txt#2.5.2.2.3
-#       //= type=implication
-#       //# The IV MUST be a unique IV within the message."""
-#     filepath: Path = Path(
-#         "/Users/tonyknap/workplace/Polymorph/aws-encryption-sdk-dafny/src"
-#         "/AwsCryptographicMaterialProviders/Client.dfy"
-#     )
-#     a_parser = AnnotationParser(paths=[filepath])
-#     lines: list[str] = annotation_end_of_file.split("\n")
-#     lines = [line + "\n" for line in lines]
-#     anno_blocks: list[LineSpan] = a_parser._extract_blocks(lines)
-#     kwargs: list[dict] = a_parser._extract_anno_kwargs(lines, anno_blocks)
-#
-#
-# def test_double_block():
-#     annotation_nested_in_function = """
-#       function method IVSeq(suite: Client.AlgorithmSuites.AlgorithmSuite, sequenceNumber: uint32)
-#         :(ret: seq<uint8>)
-#         //= compliance/data-format/message-body.txt#2.5.2.1.2
-#         //= type=implication
-#         //# The IV length MUST be equal to the IV
-#         //# length of the algorithm suite specified by the Algorithm Suite ID
-#         //# (message-header.md#algorithm-suite-id) field.
-#         //= compliance/data-format/message-body.txt#2.5.2.2.3
-#         //= type=implication
-#         //# The IV length MUST be equal to the IV length of the algorithm suite
-#         //# (../framework/algorithm-suites.md) that generated the message.
-#         ensures |ret| == suite.encrypt.ivLength as nat
-#       {
-#         seq(suite.encrypt.ivLength as int - 4, _ => 0) + UInt32ToSeq(sequenceNumber)
-#       }"""
-#     filepath: Path = Path(
-#         "/Users/tonyknap/workplace/Polymorph/aws-encryption-sdk-dafny/src"
-#         "/AwsCryptographicMaterialProviders/Client.dfy"
-#     )
-#     a_parser = AnnotationParser(paths=[filepath])
-#     lines: list[str] = annotation_nested_in_function.split("\n")
-#     lines = [line + "\n" for line in lines]
-#     anno_blocks: list[LineSpan] = a_parser._extract_blocks(lines)
-#     kwargs: list[dict] = a_parser._extract_anno_kwargs(lines, anno_blocks)
+class TestExtractkwargs:
+    test_str = "//= target\n" "//= type=implication\n" "//# Duvet MUST\n"
+    nested_str = "some code\n" "//= target\n" "//= type=implication\n" "//# Duvet MUST\n" "some code\n"
+
+    @pytest.mark.parametrize(
+        "lines, spans, expected_dicts",
+        [
+            (
+                test_str.splitlines(keepends=True),
+                [LineSpan(0, 3)],
+                [
+                    {
+                        "content": "Duvet MUST",
+                        "end_line": 3,
+                        "reason": None,
+                        "start_line": 0,
+                        "target": "target",
+                        "type": "implication",
+                    }
+                ],
+            ),
+            (
+                nested_str.splitlines(True),
+                [LineSpan(1, 4)],
+                [
+                    {
+                        "content": "Duvet MUST",
+                        "end_line": 4,
+                        "reason": None,
+                        "start_line": 1,
+                        "target": "target",
+                        "type": "implication",
+                    }
+                ],
+            ),
+            (
+                (test_str + test_str).splitlines(True),
+                [LineSpan(0, 3), LineSpan(3, 6)],
+                [
+                    {
+                        "content": "Duvet MUST",
+                        "end_line": 3,
+                        "reason": None,
+                        "start_line": 0,
+                        "target": "target",
+                        "type": "implication",
+                    },
+                    {
+                        "content": "Duvet MUST",
+                        "end_line": 6,
+                        "reason": None,
+                        "start_line": 3,
+                        "target": "target",
+                        "type": "implication",
+                    },
+                ],
+            ),
+        ],
+    )
+    def test_extract(self, under_test, lines: list[str], spans: list[LineSpan], expected_dicts: list[dict]):
+        # lines = self.test_str.splitlines(keepends=True)
+        actual_spans = under_test._extract_anno_kwargs(lines, spans)
+        assert expected_dicts == actual_spans
