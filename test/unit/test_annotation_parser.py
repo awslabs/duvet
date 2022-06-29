65115ed3883569af59a316978020c098537628df
Tony Knapp <5892063+texastony@users.noreply.github.com>
2022-06-29 11:08:35 -0700

    refactor(annotation): refactor tests and implementation (#49)

--- test/unit/test_annotation_parser.py
+++ test/unit/test_annotation_parser.py
@@ -1,15 +1,30 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Annotation Parser used by duvet-python."""
-import copy
+import logging
+from copy import deepcopy
 
 import pytest
-from _pytest.tmpdir import tmp_path
 
 from duvet._config import DEFAULT_CONTENT_STYLE, DEFAULT_META_STYLE
 from duvet.annotation_parser import AnnotationParser, LineSpan
 
 pytestmark = [pytest.mark.unit, pytest.mark.local]
+VALID_ANNO_KWARGS = {
+    "target": "target",
+    "type": "implication",
+    "start_line": 0,
+    "end_line": 3,
+    "reason": None,
+    "content": "Duvet MUST",
+}
+TEST_STR = f"{DEFAULT_META_STYLE} target\n{DEFAULT_META_STYLE} type=implication\n{DEFAULT_CONTENT_STYLE} Duvet MUST\n"
+
+
+def _update_valid_kwargs(updates: dict) -> dict:
+    rtn = deepcopy(VALID_ANNO_KWARGS)
+    rtn.update(updates)
+    return rtn
 
 
 @pytest.fixture
@@ -32,88 +47,48 @@ class TestExtractSpans:
     )
     def test_extract(self, under_test, text: str, expected_spans: list[LineSpan]):
         lines = text.splitlines(keepends=True)
-        actual_spans = under_test._extract_blocks(lines)
+        actual_spans = under_test._extract_spans(lines)
         assert expected_spans == actual_spans
 
 
-class TestExtractkwargs:
-    test_str = "//= target\n//= type=implication\n//# Duvet MUST\n"
-    nested_str = "some code\n//= target\n//= type=implication\n//# Duvet MUST\nsome code\n"
+class TestExtractKwargs:
+    nested_str = f"some code\n{TEST_STR}some code\n"
 
     @pytest.mark.parametrize(
         "lines, spans, expected_dicts",
         [
             (
-                test_str.splitlines(keepends=True),
+                TEST_STR.splitlines(True),
                 [LineSpan(0, 3)],
-                [
-                    {
-                        "content": "Duvet MUST",
-                        "end_line": 3,
-                        "reason": None,
-                        "start_line": 0,
-                        "target": "target",
-                        "type": "implication",
-                    }
-                ],
+                [deepcopy(VALID_ANNO_KWARGS)],
             ),
             (
                 nested_str.splitlines(True),
                 [LineSpan(1, 4)],
-                [
-                    {
-                        "content": "Duvet MUST",
-                        "end_line": 4,
-                        "reason": None,
-                        "start_line": 1,
-                        "target": "target",
-                        "type": "implication",
-                    }
-                ],
+                [_update_valid_kwargs({"start_line": 1, "end_line": 4})],
             ),
             (
-                (test_str + test_str).splitlines(True),
+                (TEST_STR + TEST_STR).splitlines(True),
                 [LineSpan(0, 3), LineSpan(3, 6)],
-                [
-                    {
-                        "content": "Duvet MUST",
-                        "end_line": 3,
-                        "reason": None,
-                        "start_line": 0,
-                        "target": "target",
-                        "type": "implication",
-                    },
-                    {
-                        "content": "Duvet MUST",
-                        "end_line": 6,
-                        "reason": None,
-                        "start_line": 3,
-                        "target": "target",
-                        "type": "implication",
-                    },
-                ],
+                [deepcopy(VALID_ANNO_KWARGS), _update_valid_kwargs({"start_line": 3, "end_line": 6})],
             ),
         ],
     )
-    # pylint disable=no-self-use
     def test_extract(self, under_test, lines: list[str], spans: list[LineSpan], expected_dicts: list[dict]):
         actual_spans = under_test._extract_anno_kwargs(lines, spans)
         assert expected_dicts == actual_spans
 
 
 class TestProcessKwargs:
-    def test_process_annos(self, under_test):
-        kwarg = {
-            "target": "target",
-            "type": "target",
-            "start_line": 1,
-            "end_line": 2,
-            "reason": "reason",
-            "content": "content_entry",
-        }
-        target_kwarg = copy.deepcopy(kwarg)
-        target_kwarg["target"] = None
-        type_kwarg = copy.deepcopy(kwarg)
-        type_kwarg["type"] = "none"
-        actual_result = under_test._process_anno_kwargs([target_kwarg, type_kwarg], tmp_path)
+    @pytest.mark.parametrize("kwarg", (_update_valid_kwargs({"target": None}), _update_valid_kwargs({"content": ""})))
+    def test_skips_no_content_or_no_target(self, under_test, kwarg):
+        actual_result = under_test._process_anno_kwargs([kwarg], under_test.paths[0])
         assert len(actual_result) == 0
+
+    def test_skip_and_warns_unknown_type(self, under_test, caplog):
+        kwarg = _update_valid_kwargs({"type": "Anton"})
+        with caplog.at_level(logging.WARN):
+            actual = under_test._process_anno_kwargs([kwarg], under_test.paths[0])
+            assert len(actual) == 0
+        assert len(caplog.messages) == 1
+        assert "Unknown type: Anton found in lines 0 to 3. Skipping" in caplog.messages[0]
