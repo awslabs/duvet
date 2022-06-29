eb2a0765c3724056f12a84471a230b0215891af1
Yuan Chen <18861720589@163.com>
2022-06-29 14:22:57 -0700

    feat(annotation parser): add integration test

--- test/integration/test_implementation_parsing.py
+++ test/integration/test_implementation_parsing.py
@@ -1,15 +1,13 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Integration test suite for duvet Annotation Parser."""
-
-
 import pytest
 
 from duvet.annotation_parser import AnnotationParser
 
 
 @pytest.mark.integ
-def test_extract_python_implementation_annotation(pytestconfig, caplog):
+def test_extract_python_implementation_annotation(pytestconfig):
     actual_paths = list(pytestconfig.rootpath.glob("src/**/*.py"))
     actual_paths.extend(list(pytestconfig.rootpath.glob("test/**/*.py")))
     anno_meta_style = "# //="
@@ -19,7 +17,7 @@ def test_extract_python_implementation_annotation(pytestconfig, caplog):
 
 
 @pytest.mark.integ
-def test_extract_dafny_implementation_annotation(pytestconfig, caplog):
+def test_extract_dafny_implementation_annotation(pytestconfig):
     dfy_path = pytestconfig.rootpath
     actual_paths = list(dfy_path.glob("../aws-encryption-sdk-dafny/src/**/*.dfy"))
     actual_paths.extend(list(dfy_path.glob("../aws-encryption-sdk-dafny/test/**/*.dfy")))
