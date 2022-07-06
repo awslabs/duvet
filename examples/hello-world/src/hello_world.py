291749aa4db686c6c7f7aab629a668363f3b2622
Yuan Chen <18861720589@163.com>
2022-07-06 08:13:33 -0700

    WIP feat(JSON report): add hello-world example

---
+++ examples/hello-world/src/hello_world.py
@@ -0,0 +1,9 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""Annotation Parser used by duvet-python."""
+
+
+# //= compliance/hello-world.txt#2.1
+# //# Python project MUST print hello.
+def hello_duvet():
+    print("hello duvet.")
