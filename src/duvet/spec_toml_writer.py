4d5ca63be0331224a4f1dbc775ca262d4f1c1e9f
Yuan Chen <18861720589@163.com>
2022-07-15 10:12:59 -0700

    refactor(writer): refactor TOML specification writer

--- src/duvet/spec_toml_writer.py
+++ src/duvet/spec_toml_writer.py
@@ -1,9 +1,12 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-"""Specification Parser used by duvet-python for toml format."""
+"""Specification TOML writer used by duvet-python for toml format.
+
+Assumptions:
+
+"""
 import logging
 from pathlib import Path
-from typing import Any, Dict, List, MutableMapping, Optional
 
 import toml
 from attr import define
@@ -21,13 +24,14 @@ __all__ = ["TomlRequirementWriter"]
 class TomlRequirementWriter:
     """TOML specifications writer."""
 
-    def from_section(self, section: Section, parent_path: Path) -> list[Path]:
+    @staticmethod
+    def _process_section(section: Section, parent_path: Path) -> list[Path]:
         """Write TOML from Section."""
 
         section_path: Path = parent_path.joinpath(section.title + ".toml")
         with open(section_path, "w+") as section_file:
             heading = section.uri.rsplit(".", 1)
-            target = self.url + "#" + heading[len(heading) - 1]
+            target = section.url + "#" + heading[len(heading) - 1]
             requirements: list[dict] = []
             for requirement in section.requirements.values():
                 temp_dict = {TOML_REQ_LEVEL_KEY: requirement.requirement_level.name,
@@ -35,24 +39,26 @@ class TomlRequirementWriter:
                 requirements.append(temp_dict)
 
             section_toml = {TOML_URI_KEY: target, TOML_SPEC_KEY: requirements}
-            self.dump(section_toml, section_file)
+            toml.dump(section_toml, section_file)
         return [section_path]
 
-    def from_specification(self, specification: Specification, parent_path: Path) -> list[Path]:
+    @staticmethod
+    def _process_specification(specification: Specification, parent_path: Path) -> list[Path]:
         """Write TOML from Specification."""
 
         specification_path: Path = parent_path.joinpath(specification.title)
         section_paths: list[Path] = []
 
         for section in specification.sections.values():
-            section_paths.extend(self.from_section(section, specification_path))
+            section_paths.extend(TomlRequirementWriter._process_section(section, specification_path))
         return section_paths
 
-    def from_report(self, report: Report, directory: Path) -> list[Path]:
+    @staticmethod
+    def process_report(report: Report, directory: Path) -> list[Path]:
         """Write TOML from Report."""
 
         section_paths: list[Path] = []
         for specification in report.specifications.values():
-            section_paths.extend(self.from_specification(specification, directory))
+            section_paths.extend(TomlRequirementWriter._process_specification(specification, directory))
 
         return section_paths
