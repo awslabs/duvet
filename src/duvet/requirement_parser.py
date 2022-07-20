1df235e61b4993695ba128c4148dca6bdd0da26a
Yuan Chen <18861720589@163.com>
2022-07-20 10:16:11 -0700

    WIP refactor(requirement parser): refactor requirement parser

--- src/duvet/requirement_parser.py
+++ src/duvet/requirement_parser.py
@@ -1,24 +1,16 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Requirement Parser used by duvet-python."""
-import copy
 import logging
 import re
+from abc import abstractmethod
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple
 
 from attrs import define
 
 from duvet.formatter import SENTENCE_DIVIDER, clean_content
-from duvet.identifiers import (
-    ALL_RFC_LIST_ENTRY_REGEX,
-    END_OF_LIST,
-    END_OF_SENTENCE,
-    REGEX_DICT,
-    TABLE_DIVIDER,
-    RequirementLevel,
-)
-from duvet.rfc import RFCSpecification
+from duvet.identifiers import END_OF_LIST, END_OF_SENTENCE, RequirementLevel, TABLE_DIVIDER
 from duvet.specification_parser import Span
 from duvet.structures import Report, Requirement, Section, Specification
 
@@ -282,131 +274,85 @@ class RequirementParser:
         return {"requirement_level": level}
 
     @staticmethod
+    def _process_requirements(quotes, section, regex: re.Pattern) -> Section:
+
+        blocks = RequirementParser._process_block(quotes, Span(0, len(quotes)), regex)
+
+        req_kwargs: List[dict] = RequirementParser._process_section(quotes, blocks, regex)
+
+        for kwarg in req_kwargs:
+            content: Optional[str] = kwarg.get("content")
+            if content is not None:
+                kwarg.setdefault("uri", "$".join([section.uri, content]))
+            if "span" in kwarg.keys():
+                kwarg.pop("span")
+            section.add_requirement(Requirement(**kwarg))
+
+        return section
+
+    @staticmethod
+    @abstractmethod
     def process_specifications(filepaths: list[Path], report: Optional[Report] = None) -> Report:
         """Given pattern and filepath of markdown specs.
 
         Return or create a report.
         """
-        if report is None:
-            report = Report()
-
-        specifications: list[Specification] = []
-        for filepath in filepaths:
-            specifications.append(RequirementParser._process_specification(filepath))
-
-        for specification in specifications:
-            report.add_specification(specification)
-
-        return report
 
     @staticmethod
+    @abstractmethod
     def _process_specification(specification_source: Path) -> Specification:  # pylint:disable=R0914
         """Given a filepath of a markdown spec.
 
         Return a specification or none.
         """
 
-        parser: RFCSpecification = RFCSpecification.parse(specification_source)
-        specification = Specification(
-            specification_source.name, str(specification_source.relative_to(specification_source.parent.parent))
-        )
-
-        for section in RequirementParser._process_sections(parser, specification_source):
-            if specification is not None:
-                specification.add_section(section)
-
-        return specification
-
     @staticmethod
+    @abstractmethod
     def _process_sections(parser, filepath) -> List[Section]:
-        sections: list[Section] = []
+        """Given a filepath of a markdown spec.
 
-        for descendant in parser.descendants:
-            start_line = parser.content[: descendant.body_span.start].count("\n")
-            end_line = parser.content[: descendant.body_span.end].count("\n")
-            quotes: str = copy.deepcopy(descendant.get_body())
+        Return a list of sections.
+        """
 
-            lines = quotes.splitlines()
-            lines[0] = "   ".join([descendant.number, descendant.title])
+# //= compliance/duvet-specification.txt#2.2.2
+# //= type=implication
+# //# A requirement MUST be terminated by one of the following:
 
-            section_kwarg: dict = {
-                "title": descendant.number.rstrip(". "),
-                "start_line": start_line,
-                "end_line": end_line,
-                "lines": lines,
-                "uri": "#".join([str(filepath.relative_to(filepath.parent.parent)), descendant.number.rstrip(". ")]),
-            }
+# //= compliance/duvet-specification.txt#2.2.2
+# //= type=implication
+# //# In the case of requirement terminated by a list, the text proceeding the list MUST be concatenated with each
+# //# element of the list to form a requirement.
 
-            section = Section(**section_kwarg)
+# //= compliance/duvet-specification.txt#2.2.2
+# //= type=implication
+# //# List elements MAY have RFC 2119 keywords, this is the same as regular sentences with multiple keywords.
 
-            section_with_requirements: list[Section] = []
-            if filepath.suffix == ".txt":
-                section_with_requirements.append(RequirementParser._process_requirements(quotes, section))
 
-            if filepath.suffix == ".md":
-                section_with_requirements.append(RequirementParser._process_requirements(quotes, section, "MARKDOWN"))
+# //= compliance/duvet-specification.txt#2.3.6
+# //= type=implication
+# //# A one or more line meta part MUST be followed by at least a one line content part.
 
-            sections.extend(section_with_requirements)
+# //= compliance/duvet-specification.txt#2.2.2
+# //= type=TODO
+# //# Sublists MUST be treated as if the parent item were terminated by the sublist.
 
-        return sections
 
-    @staticmethod
-    def _process_requirements(quotes, section, file_type: str = "RFC") -> Section:
-        blocks = RequirementParser._process_block(
-            quotes, Span(0, len(quotes)), REGEX_DICT.get(file_type, ALL_RFC_LIST_ENTRY_REGEX)
-        )
+# //= compliance/duvet-specification.txt#2.2.1
+# //# The name of the sections MUST NOT be nested.
 
-        req_kwargs: List[dict] = RequirementParser._process_section(
-            quotes, blocks, REGEX_DICT.get(file_type, ALL_RFC_LIST_ENTRY_REGEX)
-        )
+# //= compliance/duvet-specification.txt#2.2.1
+# //= type=exception
+# //# A requirements section MUST be the top level containing header.
 
-        for kwarg in req_kwargs:
-            content: Optional[str] = kwarg.get("content")
-            if content is not None:
-                kwarg.setdefault("uri", "$".join([section.uri, content]))
-                if "span" in kwarg.keys():
-                    kwarg.pop("span")
-                section.add_requirement(Requirement(**kwarg))
+# //= compliance/duvet-specification.txt#2.2.1
+# //= type=implication
+# //# A header MUST NOT itself be a requirement.
 
-        return section
+# //= compliance/duvet-specification.txt#2.2.1
+# //= type=TODO
+# //# A section MUST be indexable by combining different levels of naming.
 
-    # //= compliance/duvet-specification.txt#2.2.2
-    # //= type=implication
-    # //# A requirement MUST be terminated by one of the following:
-
-    # //= compliance/duvet-specification.txt#2.2.2
-    # //= type=implication
-    # //# In the case of requirement terminated by a list, the text proceeding the list MUST be concatenated with each
-    # //# element of the list to form a requirement.
-
-    # //= compliance/duvet-specification.txt#2.2.2
-    # //= type=implication
-    # //# List elements MAY have RFC 2119 keywords, this is the same as regular sentences with multiple keywords.
-
-    # //= compliance/duvet-specification.txt#2.3.6
-    # //= type=implication
-    # //# A one or more line meta part MUST be followed by at least a one line content part.
-
-    # //= compliance/duvet-specification.txt#2.2.2
-    # //= type=TODO
-    # //# Sublists MUST be treated as if the parent item were terminated by the sublist.
-
-    # //= compliance/duvet-specification.txt#2.2.1
-    # //# The name of the sections MUST NOT be nested.
-
-    # //= compliance/duvet-specification.txt#2.2.1
-    # //= type=exception
-    # //# A requirements section MUST be the top level containing header.
-
-    # //= compliance/duvet-specification.txt#2.2.1
-    # //= type=implication
-    # //# A header MUST NOT itself be a requirement.
-
-    # //= compliance/duvet-specification.txt#2.2.1
-    # //= type=TODO
-    # //# A section MUST be indexable by combining different levels of naming.
-
-    # //= compliance/duvet-specification.txt#2.2.2
-    # //= type=TODO
-    # //# Sublists MUST be treated as if the parent item were
-    # //# terminated by the sublist.
+# //= compliance/duvet-specification.txt#2.2.2
+# //= type=TODO
+# //# Sublists MUST be treated as if the parent item were
+# //# terminated by the sublist.
