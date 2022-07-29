ff28cd5a7a869417415ee7e72687430f198118bf
Yuan Chen <18861720589@163.com>
2022-07-28 21:36:28 -0700

    refactor(cli): apply code review suggestions

--- src/duvet/__init__.py
+++ src/duvet/__init__.py
@@ -1,3 +1,6 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """duvet-python."""
+from duvet.identifiers import __version__
+
+__all__ = ["__version__"]
