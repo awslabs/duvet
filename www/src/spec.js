fd82d47f87c002132a274aa74c39f4933229acb7
Cameron Bytheway <bytheway.cameron@gmail.com>
2022-10-07 16:26:37 -0600

    chore: release 0.1.1 (#87)

--- www/src/spec.js
+++ www/src/spec.js
@@ -1,5 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { DataGrid } from "@material-ui/data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
