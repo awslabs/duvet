437a2eba828708dc7376577dcdd23cfe60171241
Boquan Fang <boquanfang3@gmail.com>
2026-04-16 15:39:56 -0700

    chore(clippy): update the recent clippy change (#219)

--- duvet/src/specification/ietf/parser.rs
+++ duvet/src/specification/ietf/parser.rs
@@ -118,11 +118,7 @@ impl<T: Iterator<Item = Token>> Parser<T> {
         let mut section = core::mem::take(&mut self.section)?;
 
         // trim any trailing lines
-        loop {
-            let Some(line) = section.lines.last() else {
-                break;
-            };
-
+        while let Some(line) = section.lines.last() {
             if !line.trim().is_empty() {
                 break;
             }
